b340780cbada0bbb2f510f7f6dc74b889fff9b2f
trynobass <contact@trynobass.fr>
2015-04-19 21:49:52 +0200

    Correction du code. Ajout d'une description de la classe.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,16 +1,16 @@
-
-
-
-
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
 
+/**
+ * Classe main du programme
+ * @author trynobass
+ *
+ */
 public class MyGdxGame extends Game {
 	@Override
 	public void create () {
-		//setScreen(new CameraScreen(this)); // Carte RTM
-		setScreen(new InterfaceScreen(this)); // Interface MENU
+		setScreen(new InterfaceScreen(this));
 	}
 
 }
