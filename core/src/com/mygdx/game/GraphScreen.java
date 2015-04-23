689319d28e8bf2beb2912a240f55638d478d1997
trynobass <contact@trynobass.fr>
2015-04-23 11:43:17 +0200

    Affichage d'un chiffre généré aléatoirement dans les cercles bleu du grah. Si on touche l'écran un nouveau chiffre sera affiché.

--- core/src/com/mygdx/game/GraphScreen.java
+++ core/src/com/mygdx/game/GraphScreen.java
@@ -1,11 +1,19 @@
 package com.mygdx.game;
 
+import java.util.Random;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 import fortunes_algo.FortunesAlgorithm;
 import fortunes_algo.Site;
@@ -14,16 +22,34 @@ public class GraphScreen implements Screen {
 
 	private FortunesAlgorithm fa;
 	private ShapeRenderer sr;
+	private Label lbl;
+	private LabelStyle lblStyle;
+	private BitmapFont bFont;
+	private Stage stage;
+
+	private MyGdxGame game;
+	private Skin uiSkin;
+
+	private Random rand;
+
+	private int nbRand;
 
 	public GraphScreen(MyGdxGame game) {
+		this.game = game;
 	}
 
 	@Override
 	public void show() {
 
+		bFont = new BitmapFont(Gdx.files.internal("default.fnt"));
+		lblStyle = new LabelStyle(bFont, Color.WHITE);
+
 		Gdx.graphics.setDisplayMode(1000, 1000, false);
 
 		// Create a new set of sites
+		rand = new Random();
+		stage = new Stage();
+		lbl = new Label("1", lblStyle);
 		sr = new ShapeRenderer();
 		fa = new FortunesAlgorithm(0, 0, 1000, 1000, 20);
 		new SpriteBatch();
@@ -36,11 +62,18 @@ public class GraphScreen implements Screen {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		for (Site s : fa.sites()) {
+
 			sr.begin(ShapeRenderer.ShapeType.Filled);
 			sr.setColor(Color.BLUE);
 			sr.circle(s.location().x, s.location().y, 50);
 			sr.end();
+			
+			lbl.setPosition(s.location().x - 10, s.location().y - 10);
+			stage.addActor(lbl);
+			stage.draw();
 		}
+		
+		actionAndroid();
 
 	}
 
@@ -74,4 +107,56 @@ public class GraphScreen implements Screen {
 
 	}
 
+	public void actionAndroid(){
+		  Gdx.input.setInputProcessor(new InputProcessor() {
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		nbRand = rand.nextInt(20 - 0 + 1) + 0;
+		lbl.setText(String.valueOf(nbRand));
+		return false;
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+		  });}
 }
